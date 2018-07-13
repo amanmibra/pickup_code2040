8bc61b2c2a99706fb5899267e8d16420b89324d1
Aman Ibrahim <amanmibra@gmail.com>
2018-07-13 10:07:56 -0700

    Added classnames to event components

---
+++ src/components/AddEvent/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class AddEvent extends Component {
+  render () {
+    return (
+      <div>
+        AddEvent
+      </div>
+    );
+  }
+}
---
+++ src/components/ViewEvent/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ViewEvent extends Component {
+  render () {
+    return (
+      <div>
+        ViewEvent
+      </div>
+    );
+  }
+}
