36f836bb9a6d8ed5cb505523c48d47100ffc9a79
Aman Ibrahim <amanmibra@gmail.com>
2018-07-13 10:01:59 -0700

    set up material ui v0.20

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import logo from './../images/logo.svg';
+import theme from '../global/material-ui-theme';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          HI EVERYONE
-        </p>
-      </div>
+      <MuiThemeProvider theme={theme} >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            HI EVERYONE
+          </p>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
