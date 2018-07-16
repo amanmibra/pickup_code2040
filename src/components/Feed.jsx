54ac800f38342dda89aea1cf492094069e2d3199
Aman Ibrahim <amanmibra@gmail.com>
2018-07-16 15:00:31 -0700

    Created mock feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
 export default class Feed extends Component {
+  genCards = num => {
+    const cards = []
+    for (let i = 0; i < num; i++) {
+      cards.push(
+        <div
+          style={{
+            margin: '24px 0 24px 0'
+          }}
+        >
+          <Card>
+            <CardTitle>
+              <h3>Feed Example {'#' + i}</h3>
+            </CardTitle>
+              <CardText>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+                Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+                Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+              </CardText>
+          </Card>
+        </div>
+      )
+    }
+    return cards;
+  }
+
   render () {
     return (
-      <div>
-        Feed
+      <div
+        style={{
+          padding: '0 64px 0 64px'
+        }}
+      >
+        { this.genCards(4) }
       </div>
     );
   }
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
+import SearchBar from 'material-ui-search-bar'
+import Feed from '../components/Feed';
+
 import logo from './../images/logo.svg';
 
 class Home extends Component {
   render() {
     return (
       <div className="App">
-        Home
+        <SearchBar
+          onChange={() => console.log('onChange')}
+          onRequestSearch={() => console.log('onRequestSearch')}
+          style={{
+            margin: '0 auto',
+            maxWidth: 800
+          }}
+        />
+
+      <Feed />
       </div>
     );
   }
